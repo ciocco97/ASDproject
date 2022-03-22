4e5f39ed5cea1b8c4377014f57a4c847aaf36c7b
ciocco97 <f.cremascoli@studenti.unibs.it>
2022-03-22 16:19:38 +0000

    Fino al check

--- subqueue.py
+++ subqueue.py
@@ -3,13 +3,13 @@ from subset import Subset
 class SubsQueue:
     def __init__(self):
         self.items = []
-        self.items.insert(0, Subset(0))
+        self.items.insert(0, Subset([0]))
 
     def enqueue(self, item):
-        self.items.insert(len(self.items), item)
+        self.items.append(item)
 
     def dequeue(self):
         return self.items.pop()
 
-    def mamma(self):
+    def size(self):
         return len(self.items)
--- subset.py
+++ subset.py
@@ -1,11 +1,11 @@
 class Subset:
-    def __init__(self, *vals):
+    def __init__(self, vals):
         self.components = []
         for val in vals:
             self.components.append(val)
 
     def __str__(self):
-        text = ""
+        text = "Subset str: "
         for val in self.components:
             text = text + str(val) + " - "
         return text[:len(text) - 3]
@@ -21,7 +21,11 @@ class Subset:
             return 0
 
     def max(self):
+        # print(f"Max function: {self.components}")
         return max(self.components)
 
     def add(self, singoletto):
         self.components.append(singoletto)
+
+    def get_components(self):
+        return self.components
