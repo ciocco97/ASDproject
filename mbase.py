82d49a0bf6086c5d63129e362a0cda3678f32a8a
ciocco97 <f.cremascoli@studenti.unibs.it>
2022-03-25 09:51:01 +0100

    Ottimizzazione Non serve calcolare il vettore rappresentativo di t = delta, basta calcolare il vettore rappresentativo di delta fuori dal for

--- mbase.py
+++ mbase.py
@@ -3,7 +3,7 @@ import logging
 from data.data import Data
 from subset import Subset
 from subqueue import SubsQueue
-from representative_vector import RepresentativeVector
+from representative_vector import generate_new_rv
 import random
 
 RESULT_TO_STRING = {-1: "KO", 1: "MHS", 0: "OK"}
@@ -26,9 +26,11 @@ class MBase:
         queue = SubsQueue()
         while queue.size() > 0:
             logging.info(f"\tQueue size: {queue.size()}")
-            delta = queue.dequeue()
 
-            logging.info(f"\tdelta: {delta}")
+            delta = queue.dequeue()
+            rv1 = data.get_representative_vector(delta)
+
+            logging.info(f"\tdelta: {delta} representative vector: - {rv1}")
             logging.info(f"\tdelta_max: {delta.max()}")
             logging.info(f"\tdelta components: {delta.get_components()}, {delta.__hash__()}")
 
@@ -36,17 +38,14 @@ class MBase:
                 logging.info(f"\t\t- e: {e} in {range(delta.max() + 1, data.get_domain_size())} -")
 
                 t = Subset(delta.get_components())
-
-                logging.info(f"\t\tnew delta: {t}")
-                # optimization: the second we know it is a singlet so there is a data structure on purpose (an array) so we access it super ez pz lemon sqz
-                rv1 = data.get_representative_vector(t)
-                rv2 = data.get_singlet_representative_vector(e)
-                logging.info(f"\t\t{t} - {rv1}")
-                logging.info(f"\t\t{e} - {rv2}")
-                new_rv = self.generate_new_rv(rv1, rv2)
-                logging.info(f"\t\t{e} - {rv2}")
                 t.add(e)
-                data.add_representative_vector(t, new_rv)
+                # optimization: we know it is a singlet so there is a data structure on purpose (an array) so we access it super ez pz lemon sqz
+                rv2 = data.get_singlet_representative_vector(e)
+                logging.info(f"\t\te: {e} representative vector: - {rv2}")
+                t_rv = generate_new_rv(rv1, rv2, data.N)
+                logging.info(f"\t\tT: {t} representative vector: - {t_rv}")
+                data.add_representative_vector(t, t_rv)
+
                 result = self.the_best_check_in_the_entire_world(t)
                 logging.info(f"\t\tresult: {RESULT_TO_STRING[result]}")
 
@@ -57,15 +56,6 @@ class MBase:
                     logging.info(f"\t\tMinimal hitting set: {t}")
 
         print(*(x for x in self.output), sep='\n')
-
-    # this procedure generate the new rv starting from a couple
-    def generate_new_rv(self, rv1: RepresentativeVector, rv2: RepresentativeVector) -> RepresentativeVector:
-        new = RepresentativeVector(self.data.N)
-        for i, phi1 in enumerate(rv1.get_values()):
-            phi2 = rv2.get_values()[i]
-            result = phi1 + phi2
-            new.set_val_by_index(i, result if 0 <= result <= max(phi1, phi2) else RepresentativeVector.X_VAL)
-        return new
 
     def the_best_check_in_the_entire_world(self, t):
         rv = self.data.get_representative_vector(t)
--- representative_vector.py
+++ representative_vector.py
@@ -19,3 +19,13 @@ class RepresentativeVector:
 
     def get_values(self):
         return self.vector
+
+
+# this procedure generate the new rv starting from a couple
+def generate_new_rv(rv1: RepresentativeVector, rv2: RepresentativeVector, num_of_sets: int) -> RepresentativeVector:
+    new = RepresentativeVector(num_of_sets)
+    for i, phi1 in enumerate(rv1.get_values()):
+        phi2 = rv2.get_values()[i]
+        result = phi1 + phi2
+        new.set_val_by_index(i, result if 0 <= result <= max(phi1, phi2) else RepresentativeVector.X_VAL)
+    return new
