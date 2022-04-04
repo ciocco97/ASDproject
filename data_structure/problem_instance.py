7f3bd1f2745031f46ef2d5d03c3becefabc21c1d
AlessandroTrainini <alesstra01@hotmail.it>
2022-04-04 13:28:18 +0200

    Optimized the memory usage and computational time by using lighter data structure (without using any objects)

--- data_structure/problem_instance.py
+++ data_structure/problem_instance.py
@@ -1,13 +1,11 @@
-from data_structure.representative_vector import RepresentativeVector
-from data_structure.subset import Subset
-import logging
-
 
 class ProblemInstance:
 
     def __init__(self, matrix_one_zero):
         self.M = 0
         self.N = 0
+
+        self.X_VAL = -1
 
         self.matrix_lex = []
         self.singlet_representative_vectors = []
@@ -23,17 +21,16 @@ class ProblemInstance:
         self.set_matrix_lex(matrix_one_zero)
         # logging.info(f"List of Subsets: {self.matrix_lex}")
 
-        RepresentativeVector.X_VAL = -1 * (self.M + 1)
+        self.X_VAL = -1 * (self.M + 1)
         # the representative vector of the empty subset is the null vector of size N
-        rv_zero = RepresentativeVector([0]*self.N)
-        self.add_singlet_rv(0, rv_zero)
+        self.add_singlet_rv(0, [0]*self.N)
 
         # computation of the representative vectors of the singletties :')
         for singlet in range(1, self.M + 1):
             values = [0] * self.N
             for i, N_i in enumerate(self.matrix_lex):
                 values[i] = singlet if singlet in N_i else 0
-            self.add_singlet_rv(singlet, RepresentativeVector(values))
+            self.add_singlet_rv(singlet, values.copy())
 
     def set_M(self, M):
         self.M = M
@@ -45,20 +42,20 @@ class ProblemInstance:
         for row in matrix_one_zero:
             self.matrix_lex.append([k + 1 for k, x in enumerate(row) if x == 1])
 
-    def add_singlet_rv(self, singlet: int, representative_vector: RepresentativeVector):
+    def add_singlet_rv(self, singlet: int, representative_vector: []):
         self.singlet_representative_vectors.insert(singlet, representative_vector)
 
-    def add_rv(self, sigma: Subset, representative_vector: RepresentativeVector):
-        self.representative_vectors[str(sigma)] = representative_vector
+    def add_rv(self, sigma: [], representative_vector: []):
+        self.representative_vectors[str(sigma)] = representative_vector.copy()
 
-    def get_rv(self, sigma: Subset) -> RepresentativeVector:
-        size = sigma.get_size()
+    def get_rv(self, sigma) -> []:
+        size = len(sigma)
         if size > 1:
             return self.representative_vectors[str(sigma)]
         elif size == 1:
-            return self.singlet_representative_vectors[sigma.get_components()[0]]
+            return self.singlet_representative_vectors[sigma[0]]
         else:
             return self.singlet_representative_vectors[0]
 
-    def get_singlet_rv(self, singlet: int) -> RepresentativeVector:
+    def get_singlet_rv(self, singlet: int) -> []:
         return self.singlet_representative_vectors[singlet]
