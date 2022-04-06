952a613bb07849234434968e92b691e26c136e44
AlessandroTrainini <alesstra01@hotmail.it>
2022-04-06 11:20:42 +0200

    Another optimization, now the singlets rv are both in the tuple and in the dictionary with all the others rvs, to improve the get_rv() method

--- data_structure/problem_instance.py
+++ data_structure/problem_instance.py
@@ -1,4 +1,3 @@
-
 class ProblemInstance:
 
     def __init__(self, matrix_one_zero):
@@ -8,8 +7,8 @@ class ProblemInstance:
         self.X_VAL = -1
 
         self.matrix_lex = []
-        self.singlet_representative_vectors = []
-        self.representative_vectors = {}
+        self.singlet_representative_vectors = None
+        self.representative_vectors = dict()
 
         self.conf(matrix_one_zero)
 
@@ -23,14 +22,16 @@ class ProblemInstance:
 
         self.X_VAL = -1 * (self.M + 1)
         # the representative vector of the empty subset is the null vector of size N
-        self.add_singlet_rv(0, [0]*self.N)
-
+        singlet_rvs = [[0] * self.N]
+        self.add_rv([], tuple([0] * self.N))
         # computation of the representative vectors of the singletties :')
         for singlet in range(1, self.M + 1):
             values = [0] * self.N
             for i, N_i in enumerate(self.matrix_lex):
                 values[i] = singlet if singlet in N_i else 0
-            self.add_singlet_rv(singlet, values.copy())
+            singlet_rvs.append(values)
+            self.add_rv([singlet], tuple(values))
+        self.singlet_representative_vectors = tuple(singlet_rvs)
 
     def set_M(self, M):
         self.M = M
@@ -42,20 +43,11 @@ class ProblemInstance:
         for row in matrix_one_zero:
             self.matrix_lex.append([k + 1 for k, x in enumerate(row) if x == 1])
 
-    def add_singlet_rv(self, singlet: int, representative_vector: []):
+    def add_singlet_rv(self, singlet: int, representative_vector: tuple):
         self.singlet_representative_vectors.insert(singlet, representative_vector)
 
-    def add_rv(self, sigma: [], representative_vector: []):
-        self.representative_vectors[str(sigma)] = representative_vector.copy()
+    def add_rv(self, sigma: [], representative_vector: tuple):
+        self.representative_vectors[str(sigma)] = representative_vector
 
-    def get_rv(self, sigma) -> []:
-        size = len(sigma)
-        if size > 1:
-            return self.representative_vectors[str(sigma)]
-        elif size == 1:
-            return self.singlet_representative_vectors[sigma[0]]
-        else:
-            return self.singlet_representative_vectors[0]
-
-    def get_singlet_rv(self, singlet: int) -> []:
-        return self.singlet_representative_vectors[singlet]
+    def get_rv(self, sigma):
+        return self.representative_vectors[str(sigma)]
